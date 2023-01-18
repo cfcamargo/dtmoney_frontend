98ca08e704d709ba9058eec2e85e4b7352f0b038
Cristian Camargo <contato@camargodev.com.br>
2023-01-18 15:03:48 -0400

    feat : add search function in transactions

--- components/molecules/Search.vue
+++ components/molecules/Search.vue
@@ -1,6 +1,6 @@
 <template>
-    <form @submit.prevent="searchTransactions" class="flex w-full gap-4 mt-16">
-        <input type="text" v-model="search" class="flex-1 w-full p-4 bg-zinc-900 text-zinc-200 placeholder:text-zinc-600 focus:ring-emerald-600 focus:outline-emerald-600" placeholder="Busque uma transação">
+    <form @submit.prevent="searchTransactions, search" class="flex w-full gap-4 mt-16">
+        <input @input="searchTransactions" @keyup.esc="cancelSearch" type="text" v-model="search" class="flex-1 w-full p-4 bg-zinc-900 text-zinc-200 placeholder:text-zinc-600 focus:ring-emerald-600 focus:outline-emerald-600" placeholder="Busque uma transação">
         <button class="flex gap-3 justify-center items-center border border-emerald-600 text-emerald-600 px-8 fill-current rounded hover:bg-emerald-600 hover:text-zinc-100 hover:fill-current">
             <img src="../../assets/images/search.svg" alt="">
             <span>Buscar</span>
@@ -18,8 +18,12 @@ export default defineComponent({
         }
     },
     methods: {
+        cancelSearch(){
+            this.search = ''
+            this.searchTransactions()
+        },
         searchTransactions(){
-            console.log('buscar')
+            this.$emit('searchTransactions', this.search)
         }
     }
 })
--- components/organisms/Transactions.vue
+++ components/organisms/Transactions.vue
@@ -1,9 +1,16 @@
 <template>
     <Container>
-        <Search />
+        <Search @searchTransactions="searchTransactions($event)"/>
         <ul class="pt-6 flex flex-col gap-2">
-            <li v-for="(transaction, index) in transactions" :key="index">
+            <li v-if="!filter" v-for="(transaction, index) in transactions" :key="index">
                 <transaction-item :transaction="transaction"/>
+            </li>
+            <li v-if="filter && filtered_transactions.length !== 0" v-for="(transaction, index) in filtered_transactions" :key="index">
+                <transaction-item :transaction="transaction"/>
+
+            </li>
+            <li v-if="filter && filtered_transactions.length === 0" class="w-full py-4 flex items-center justify-center">
+                <span class="text-center text-zinc-300">Nada Encontrado...</span>
             </li>
         </ul>
         <div class="absolute bottom-0 left-1/2 -translate-x-1/2">
@@ -26,7 +33,18 @@ export default {
 
     data(){
         return {
-            transactions : []
+            filtered_transactions : [] as Transaction[],
+            filter: false
+        }
+    },
+    methods: {
+        searchTransactions(param : string){
+            if(!param){
+                this.filter=false
+            } else {
+                this.filtered_transactions = this.transactions.filter(transaction => transaction.description.includes(param))
+                this.filter=true
+            }
         }
     },
     computed: {
